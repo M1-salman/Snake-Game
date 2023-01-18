d4035859b74f9fa0ecfe525a9f1d891810bb4e96
Salman Masood <122998819+M1-salman@users.noreply.github.com>
2023-01-18 21:57:56 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,159 @@
+// Game Constants & Variables
+let inputDir = {x:0, y:0};
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio ('music/gameover.mp3');
+const moveSound = new Audio ('music/move.mp3');
+const musicSound= new Audio ('music/music.mp3');
+let speed = 5;
+let score = 0;
+let lastPaintTime = 0 ;
+let snakeArr = [ {x:13, y:15} ]
+let food = {x:6, y:7};
+
+// creating food element
+foodElement = document.createElement('div');
+
+
+//Game Functions 
+function main(ctime){
+    window.requestAnimationFrame(main);
+    // console.log(ctime)
+    if((ctime-lastPaintTime)/1000 <1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+
+function isCollide(snake){
+    // if you bump into yourself
+    for (let i = 1; i < snakeArr.length; i++) {
+        if (snake[i].x ===snake[0].x && snake[i].y ===snake[0].y){
+        return true;
+        }   
+    }
+    // if you bump into the wall
+    if (snake[0].x >= 18 || snake[0].x <= 0 && snake[0].y >=18 || snake[0].y <=0){
+        return true;
+    }
+}
+
+function gameEngine(){
+    // Part 1: Updating the snake array & food
+    if(isCollide(snakeArr)){
+      gameOverSound.play();
+      musicSound.pause();
+      inputDir = {x:0, y:0};
+      alert('Game Over. Press Enter key to play again !');
+      snakeArr = [ {x:13, y:15} ];
+      musicSound.play();
+      score = 0;
+    }
+
+    // if you have eaten the food,increment the score and regenerate the food 
+    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
+        foodSound.play();
+        score += 1;
+        if(score>hiscoreval){
+            hiscoreval = score;
+            localStorage.setItem("hisc0re", JSON.stringify(hiscoreval));
+            hiScoreBox.innerHTML = "HighScore: " + hiscoreval;
+        }
+        scoreBox.innerHTML = 'Your Score: '+score;
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
+        let a = 2;
+        let b = 16;
+        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+    }
+
+    // Moving the snake
+    for (let i = snakeArr.length - 2; i>=0; i--) { 
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
+
+    //Part 2: Display the snake and food
+    // Display the snake 
+    board.innerHTML = ""; 
+    snakeArr.forEach((e,index)=>{
+       snakeElement = document.createElement('div');
+       snakeElement.style.gridRowStart = e.y;
+       snakeElement.style.gridColumnStart = e.x;
+       if(index===0){
+        snakeElement.classList.add('head');
+       }
+       else{
+        snakeElement.classList.add('snake');
+       }
+       board.appendChild(snakeElement);
+    })
+
+    //Display the food
+    // foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement);
+    
+    
+}
+
+//Main logic starts here
+let Hiscore = localStorage.getItem("hisc0re");
+if(Hiscore === null){
+    hiscoreval = 0;
+    localStorage.setItem("hisc0re", JSON.stringify(hiscoreval))
+}
+else{
+    hiscoreval = JSON.parse(Hiscore);
+    hiScoreBox.innerHTML = "HighScore: " + Hiscore;
+}
+window.requestAnimationFrame(main);
+start.addEventListener('click',()=>{
+    start.style.opacity ="0%"
+    foodElement.style.animation = "Food 1s linear infinite"
+    musicSound.play();
+    window.addEventListener('keydown', e =>{
+        // inputDir = {x: 0, y: 1} // Start the game
+        moveSound.play();
+        switch (e.key) {
+            case "ArrowUp":
+                console.log("ArrowUp");
+                inputDir.x = 0;
+                inputDir.y = -1;
+                break;
+    
+            case "ArrowDown":
+                console.log("ArrowDown");
+                inputDir.x = 0;
+                inputDir.y = 1;
+                break;
+    
+            case "ArrowLeft":
+                console.log("ArrowLeft");
+                inputDir.x = -1;
+                inputDir.y = 0;
+                break;
+    
+            case "ArrowRight":
+                console.log("ArrowRight");
+                inputDir.x = 1;
+                inputDir.y = 0;
+                break;
+            default:
+                break;
+        }
+    
+    });
+})
+
+// displaying How to Play ?
+info_btn.addEventListener('click',()=>{
+    console.log('salman')
+    infoCont = document.querySelector(".info_Container");
+    infoCont.classList.toggle("info-ani");
+})
+
+
